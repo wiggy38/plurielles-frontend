3e4e102d8cbe3f14ac6fc6e2c8223cea934fe3d6
manajaga <marrivenkatramanar@gmail.com>
2022-06-14 17:14:51 +0530

    'new'

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lbd-free-angular-cli'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/lbd-free-angular-cli/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
